aa61016b94a8e407c464a794962e62f933c8cb39
rakibulhasan044 <rhrakib044@gmail.com>
2024-03-04 01:14:01 +0600

    1st section design done

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const allPost = async() => {
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
+    const data = await res.json();
+    data.forEach(element => {
+        console.log(element);
+    });
+}
+
+allPost();
